51ec551e568239e6ae79ab6dee50f4791c8b49be
Yashoda Bhandari <bhandariyashoda18@gmail.com>
2026-05-19 03:37:46 +0530

    Update PortfolioPage.tsx

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -27,15 +27,6 @@ const livingRoomBase = [
   "https://i.postimg.cc/9fdRVd5v/entrance1.png",
   "https://i.postimg.cc/7L3GD3rZ/entrance2.jpg",
   "https://i.postimg.cc/Sx9d2ynr/Untitled-design4.png",
-  "https://i.postimg.cc/hf6Vs7Ms/IMG-20260506-WA0022.jpg",
-  "https://i.postimg.cc/SJMny17c/IMG-3880.avif",
-  "https://i.postimg.cc/hfdXSZb7/IMG-3943.avif",
-  "https://i.postimg.cc/jw3DjGFy/IMG-6104.avif",
-  "https://i.postimg.cc/wywmmt7g/IMG-7413.avif",
-  "https://i.postimg.cc/6707k7VS/IMG-7431.avif",
-  "https://i.postimg.cc/xJ1kn0t8/IMG-8354.avif",
-  "https://i.postimg.cc/nCjDXhZK/IMG-8375.avif",
-  "https://i.postimg.cc/8FDJXdvc/IMG-8418.avif",
 ];
 const livingRoom: Project[] = livingRoomBase.map((src, i) => ({
   src,
